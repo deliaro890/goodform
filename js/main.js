9145e988efde3c4e8c1fd5c306059ba0fc44bea1
Delia Roman <deliaroman@Delias-MacBook-Air.local>
2024-05-19 01:37:14 +0100

    first mobile design

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 function goToPage(id) {
-    window.location.assign(id+'.html');
+  window.location.assign(id+'.html');
+}
+
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.brand');
+  if (window.scrollY > 0) {
+      navbar.classList.add('transparent');
+  } else {
+      navbar.classList.remove('transparent');
   }
+});
